a3466dd35cb76218b6a735a1fc41fd52f60cc912
EESHAN GARG <149296846+Die-Another-Day@users.noreply.github.com>
2026-05-22 18:54:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -496,6 +496,6 @@ wss.on('connection', ws => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`⚡ MULTIPLAYER PSYCH — http://localhost:${PORT}`);
 });
