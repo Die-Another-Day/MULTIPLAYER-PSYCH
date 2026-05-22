a50e134231b9d8f6650bcb930a20a573177c62f9
EESHAN GARG <149296846+Die-Another-Day@users.noreply.github.com>
2026-05-22 18:41:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const path      = require('path');
 const app    = express();
 const server = http.createServer(app);
 const wss    = new WebSocket.Server({ server });
-const PORT   = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
